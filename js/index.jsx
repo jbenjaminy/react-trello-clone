dc8cb4f864600525ceff51704a401334f57ca794
Alex Bang (MBP) <alexbang@hotmail.com>
2016-07-19 17:07:02 -0400

    Add props to components

--- js/index.jsx
+++ js/index.jsx
@@ -3,48 +3,54 @@ var ReactDOM = require('react-dom');
 
 // COMPONENT
 
-var Card = function() {
-    var title = 'call mom';
+var Card = function(cardProps) {
     return (
         <div className="card">
-            {title}
+            {cardProps.text}
         </div>
     );
 };
 
-var CardList = React.createClass({
-  render: function() {
-    var cards = [];
-    for (var i = 0; i < 5; i++) {
-      cards.push(<Card/>);
-    }
+var List = function(listProps) {
     return (
-      <section className="card-list">
-        <header>To Do</header>
-        {cards}
+      <section className="list">
+        <header>{listProps.title}</header>
+        {listProps.cards}
       </section>
     );
-  }
-});
+  };
 
-var Board = React.createClass({
-  render: function() {
-    var lists = [];
-    for (var i = 0; i <3; i++) {
-      lists.push(<CardList/>);
-    }
+var Board = function(boardProps) {
     return (
       <section className="board">
-        <header>Things to Get Done</header>
-        {lists}
+        <header>{boardProps.title}</header>
+        {boardProps.lists}
         </section>
     );
-  }
-});
+  };
+
+var App = function() {
+
+var list1 = <List title="To-do"
+cards={[<Card text="Return shoes" />,<Card text="Call Mom" />,<Card text="Book flight" />,<Card text="Pick up dry cleaning" />]} />;
+
+var list2 = <List title="Doing" cards={[<Card text="Laundry" />]} />
+
+var list3 = <List title="Done" cards={[<Card text="Go to the bank" />,<Card text="Get a haircut" />,<Card text="Schedule a band practice" />,<Card text="Fix bathroom cabinet" />]} />
+
+  return (
+    <section className="app">
+      <Board title="Things to Get Done"
+             lists={[list1, list2, list3]}
+      />
+    </section>
+
+  );
+};
 
 // Wait for DOM to load
 document.addEventListener('DOMContentLoaded', function() {
     // Call the ReactDOM.render function (root component, which you want to render; place you want to render it)
     ReactDOM.render(
-        <Board/>, document.getElementById('app'));
+        <App/>, document.getElementById('app'));
 });
