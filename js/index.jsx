3144614183a8a0259507003d0dc1dcab8f864b95
Alex Bang (MBP) <alexbang@hotmail.com>
2016-07-19 15:18:13 -0400

    Add components

--- js/index.jsx
+++ js/index.jsx
@@ -2,40 +2,49 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 // COMPONENT
-var Person = function() {
-    // Variables
-    var name = 'Derek Zoolander';
-    var imageUrl = 'http://uifaces.com/assets/static/images/zoolander.jpg';
-    var job = 'Male model';
-    // Returns React element
+
+var Card = function() {
+    var title = 'call mom';
     return (
-        <div className="person">
-            <div className="person-name">{name}</div>
-            <img className="person-img" src={imageUrl}/>
-            <div className="person-job">
-                {job}
-            </div>
+        <div className="card">
+            {title}
         </div>
     );
 };
 
-var PersonList = React.createClass({
-    render: function() {
-        var people = [];
-        for (var i=0; i<5; i++) {
-            people.push(<Person />);
-        }
-        return (
-            <div className="person-list">
-                {people}
-            </div>
-        );
+var CardList = React.createClass({
+  render: function() {
+    var cards = [];
+    for (var i = 0; i < 5; i++) {
+      cards.push(<Card/>);
     }
+    return (
+      <section className="card-list">
+        <header>To Do</header>
+        {cards}
+      </section>
+    );
+  }
+});
+
+var Board = React.createClass({
+  render: function() {
+    var lists = [];
+    for (var i = 0; i <3; i++) {
+      lists.push(<CardList/>);
+    }
+    return (
+      <section className="board">
+        <header>Things to Get Done</header>
+        {lists}
+        </section>
+    );
+  }
 });
 
 // Wait for DOM to load
 document.addEventListener('DOMContentLoaded', function() {
     // Call the ReactDOM.render function (root component, which you want to render; place you want to render it)
     ReactDOM.render(
-        <PersonList/>, document.getElementById('app'));
+        <Board/>, document.getElementById('app'));
 });
