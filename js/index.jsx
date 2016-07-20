8cf8d569626ba9cc7cd5552ecb4203195b9052a8
Alex Bang (MBP) <alexbang@hotmail.com>
2016-07-20 15:39:47 -0400

    Create stateful components

--- js/index.jsx
+++ js/index.jsx
@@ -3,84 +3,94 @@ var ReactDOM = require('react-dom');
 
 // COMPONENTS
 var Input = function(inputProps) {
-  return <input onChange={inputProps.onChange} />;
+    return <input onChange={inputProps.onChange}/>;
 };
 
 var Button = function(buttonProps) {
-  return <input onClick={buttonProps.onClick} />;
+    return <input onClick={buttonProps.onClick}/>;
 };
 
 var Card = function(cardProps) {
     return (
-            <li>{cardProps.text}</li>
+        <li>{cardProps.text}</li>
     );
 };
 
-var List = function(listProps) {
-  getInitialState: function() {
-    return {clicked: false} {input: null};
-  },
+var ListContainer = React.createClass({
+    getInitialState: function() {
+        return {clicked: false, input: ""};
+    },
 
-  onAddInputChanged: function() {
-    this.setState({
-      input: find(listProps.input.val());
-    });
-  },
+    onAddInputChanged: function() {
 
-  onAddClick: function() {
-    this.setState({
-      clicked: true
-    });
-  },
+        this.setState({
+            input: find(this.props.input.val())
+        });
+    },
 
-  // render new card
+    onAddClick: function() {
+        this.setState({clicked: true});
+    },
 
-    return (
-      <div className="list-element">
-        <section className="list">
-          <header>{listProps.title}</header>
-          <ul className="list-UL">{listProps.cards}</ul>
-        </section>
-        <form>
-            {listProps.input}
-            <br />
-            {listProps.button}
-        </form> 
-      </div>
-    );
-  };
+    render: function() {
+      console.log(this.state, '<-- this.state');
+      return (
+        <div className="list-element">
+            <section className="list">
+                <header>{this.props.title}</header>
+                <ul className="list-UL">{this.props.cards}</ul>
+            </section>
+            <form>
+                {this.props.input}
+                <br/>
+                {this.props.button}
+            </form>
+        </div>
+      );
+    }
 
-var Board = function(boardProps) {
-    return (
-      <section className="board">
-        <header>{boardProps.title}</header>
-        {boardProps.lists}
-        </section>
-    );
-  };
+});
 
-var App = function() {
+// render new card
 
-var list1 = <List title="To-do"
-cards={[<Card text="Return shoes" />,<Card text="Call Mom" />,<Card text="Book flight" />,<Card text="Pick up dry cleaning" />]} input={<input type="text" name="add-text" class="add-text" placeholder="New Card" value=null/>} button={<input type="submit" class="add-button" value="Add"/>}/>
+var Board = React.createClass ({
 
-var list2 = <List title="Doing" cards={[<Card text="Laundry" />]} input={<input type="text" name="add-text" class="add-text" placeholder="New Card" value=null/>} button={<input type="submit" class="add-button" value="Add"/>} />
 
-var list3 = <List title="Done" cards={[<Card text="Go to the bank" />,<Card text="Get a haircut" />,<Card text="Schedule a band practice" />,<Card text="Fix bathroom cabinet" />]} input={<input type="text" name="add-text" class="add-text" placeholder="New Card" value=null/>} button={<input type="submit" class="add-button" value="Add"/>}/>
+
+render: function() {
+
+  var input = < input type="text" name="add-text" className="add-text" placeholder="New Card" onChange={this.onAddInputChanged} />;
+  var button = <input type="submit" className="add-button" value="Add" onClick={this.onAddClick} />;
+  var cards2 = [< Card key="2" text="Laundry" />];
+  var list2 = <ListContainer
+  title={this.props.lists[0]}
+  key="list2"
+  cards={cards2}
+  input={input}
+  button={button}
+  />;
 
   return (
-    <section className="app">
-      <Board title="Things to Get Done"
-             lists={[list1, list2, list3]}
-      />
-    </section>
-
+      <section className="board">
+                  <header>{this.props.title}</header>
+                  {list2}
+      </section>
   );
-};
+}
+});
+
+
+  //   var list1 = <List title="To-do" cards={[ < Card text = "Return shoes" />, < Card text = "Call Mom" />, < Card text = "Book flight" />, < Card text = "Pick up dry cleaning" />
+  // ]} input={< input type = "text" name = "add-text" className = "add-text" placeholder = "New Card" value = "" />} button={< input type = "submit" className = "add-button" value = "Add" />}/>
+
+
+
+  //   var list3 = <List title="Done" cards={[ < Card text = "Go to the bank" />, < Card text = "Get a haircut" />, < Card text = "Schedule a band practice" />, < Card text = "Fix bathroom cabinet" />
+  // ]} input={< input type = "text" name = "add-text" className = "add-text" placeholder = "New Card" value = "" />} button={< input type = "submit" className = "add-button" value = "Add" />}/>
 
 // Wait for DOM to load
 document.addEventListener('DOMContentLoaded', function() {
     // Call the ReactDOM.render function (root component, which you want to render; place you want to render it)
     ReactDOM.render(
-        <App/>, document.getElementById('app'));
+        <Board title="Things to Get Done" lists={["Doing"]}/>, document.getElementById('app'));
 });
